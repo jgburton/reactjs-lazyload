410aaa6584c10e9eccd9bf837a43809a0ce0b2de
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2020-01-24 14:49:33 +0000

    Experimenting with lazy loading in react js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React, { Suspense, lazy } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// import Artists from './Artists';
+const Artists = React.lazy(() => import('./Artists'));
+const Performers = lazy(() => import("./Performers"))
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Suspense fallback={<h1>Still Loading…</h1>}>
+                    <Artists />
+                    <Performers />
+                </Suspense>
+
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
